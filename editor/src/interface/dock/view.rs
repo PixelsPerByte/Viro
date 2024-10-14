db56c02444a5e0ae7a45133d78d3d4281971a0fc
PixelDoted <isaac@marketplaceplus.com>
2024-10-14 11:51:14 -0500

    Made the Camera settings functional

--- editor/src/interface/dock/view.rs
+++ editor/src/interface/dock/view.rs
@@ -1,6 +1,14 @@
 use bevy::{
-    core_pipeline::{bloom::BloomSettings, dof::DepthOfFieldSettings, motion_blur::MotionBlur},
-    pbr::{ScreenSpaceAmbientOcclusionSettings, ScreenSpaceReflectionsSettings},
+    core_pipeline::{
+        bloom::BloomSettings,
+        dof::DepthOfFieldSettings,
+        motion_blur::MotionBlur,
+        prepass::{DeferredPrepass, DepthPrepass, MotionVectorPrepass, NormalPrepass},
+    },
+    pbr::{
+        ScreenSpaceAmbientOcclusionBundle, ScreenSpaceAmbientOcclusionSettings,
+        ScreenSpaceReflectionsSettings,
+    },
     prelude::*,
 };
 use bevy_egui::egui;
@@ -19,18 +27,71 @@ pub fn settings(world: &mut World, ui: &mut egui::Ui) {
 
     ui.heading("Graphics");
 
-    ui.checkbox(&mut entity.contains::<BloomSettings>(), "Bloom");
-    ui.checkbox(
-        &mut entity.contains::<ScreenSpaceAmbientOcclusionSettings>(),
-        "SSAO",
-    );
-    ui.checkbox(
-        &mut entity.contains::<ScreenSpaceReflectionsSettings>(),
-        "SSR",
-    );
-    ui.checkbox(
-        &mut entity.contains::<DepthOfFieldSettings>(),
-        "Depth Of Field",
-    );
-    ui.checkbox(&mut entity.contains::<MotionBlur>(), "Motion Blur");
+    let mut has_bloom = entity.contains::<BloomSettings>();
+    let mut has_ssao = entity.contains::<ScreenSpaceAmbientOcclusionSettings>();
+    let mut has_ssr = entity.contains::<ScreenSpaceReflectionsSettings>();
+    let mut has_dof = entity.contains::<DepthOfFieldSettings>();
+    let mut has_motionblur = entity.contains::<MotionBlur>();
+
+    if ui.checkbox(&mut has_bloom, "Bloom").clicked() {
+        if has_bloom {
+            entity.insert(BloomSettings::default());
+        } else {
+            entity.remove::<BloomSettings>();
+        }
+    }
+
+    if ui.checkbox(&mut has_ssao, "SSAO").clicked() {
+        if has_ssao {
+            entity.insert(ScreenSpaceAmbientOcclusionSettings::default());
+        } else {
+            entity.remove::<ScreenSpaceAmbientOcclusionSettings>();
+        }
+    }
+
+    if ui.checkbox(&mut has_ssr, "SSR").clicked() {
+        if has_ssr {
+            entity.insert(ScreenSpaceReflectionsSettings::default());
+        } else {
+            entity.remove::<ScreenSpaceReflectionsSettings>();
+        }
+    }
+
+    if ui.checkbox(&mut has_dof, "Depth Of Field").clicked() {
+        if has_dof {
+            entity.insert(DepthOfFieldSettings::default());
+        } else {
+            entity.remove::<DepthOfFieldSettings>();
+        }
+    }
+
+    if ui.checkbox(&mut has_motionblur, "Motion Blur").clicked() {
+        if has_motionblur {
+            entity.insert(MotionBlur::default());
+        } else {
+            entity.remove::<MotionBlur>();
+        }
+    }
+
+    // Prepasses
+    let has_depth = entity.contains::<DepthPrepass>();
+    let has_normal = entity.contains::<NormalPrepass>();
+    let has_motion = entity.contains::<MotionVectorPrepass>();
+    let has_deferred = entity.contains::<DeferredPrepass>();
+
+    if !has_depth && (has_ssao || has_ssr || has_motionblur) {
+        entity.insert(DepthPrepass);
+    }
+
+    if !has_normal && (has_ssao) {
+        entity.insert(NormalPrepass);
+    }
+
+    if !has_motion && (has_motionblur) {
+        entity.insert(MotionVectorPrepass);
+    }
+
+    if !has_deferred && (has_ssr) {
+        entity.insert(DeferredPrepass);
+    }
 }
