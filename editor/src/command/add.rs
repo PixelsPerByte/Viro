c72bc1e3f50c5d2b06f342bd74762e0413450ee7
PixelDoted <isaac@marketplaceplus.com>
2024-10-14 19:54:57 -0500

    Create `StandardMaterial` when using the add commands

--- editor/src/command/add.rs
+++ editor/src/command/add.rs
@@ -1,8 +1,13 @@
 use bevy::prelude::*;
 
-pub fn system<M: Into<Mesh> + Default>(mut meshes: ResMut<Assets<Mesh>>, mut commands: Commands) {
+pub fn system<M: Into<Mesh> + Default>(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut commands: Commands,
+) {
     commands.spawn(PbrBundle {
         mesh: meshes.add(M::default()),
+        material: materials.add(StandardMaterial::default()),
         ..default()
     });
 }
