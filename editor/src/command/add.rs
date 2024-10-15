7b313141be15c40561025a152d65909784b814f5
PixelDoted <isaac@marketplaceplus.com>
2024-10-15 07:58:25 -0500

    Using an add command (to add a mesh) now adds a `Name` to the entity (if found)

--- editor/src/command/add.rs
+++ editor/src/command/add.rs
@@ -1,13 +1,17 @@
 use bevy::prelude::*;
 
-pub fn system<M: Into<Mesh> + Default>(
+pub fn system<M: Into<Mesh> + TypePath + Default>(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut commands: Commands,
 ) {
-    commands.spawn(PbrBundle {
+    let mut entity_cmds = commands.spawn(PbrBundle {
         mesh: meshes.add(M::default()),
         material: materials.add(StandardMaterial::default()),
         ..default()
     });
+
+    if let Some(ident) = M::type_ident() {
+        entity_cmds.insert(Name::new(ident));
+    }
 }
